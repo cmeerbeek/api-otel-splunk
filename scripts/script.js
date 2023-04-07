f0d7abfad4e9851269130effbbbbd352ab642e4e
Coen Meerbeek <cmeerbeek@gmail.com>
2023-04-07 22:47:45 +0200

    k6: fixed scripts results file and changed test

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  stages: [
+    { duration: '1m', target: 5 }, // simulate ramp-up of traffic from 1 to 30 users over 1 minute.
+    { duration: '3m', target: 5 }, // stay at 30 users for 3 minutes
+    { duration: '1m', target: 0 }, // ramp-down to 0 users
+  ],
+
+  thresholds: {
+    'http_req_duration': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
 export default function () {
   http.get('http://kong:8000/query/api/v1/test1');
   sleep(1);
